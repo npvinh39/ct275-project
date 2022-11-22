9d1ab56f0c0ca381437141c3533c0c12cf7cce91
Nguyen Phuc Vinh <nguyenphucvinh9x@gmail.com>
2022-11-23 02:34:27 +0700

    Modified Header Footer

--- view/client/pages/contact.php
+++ view/client/pages/contact.php
@@ -15,35 +15,8 @@
 <body>
   <div class="app">
     <div class="header-blue py-0">
-      <nav class="navbar navbar-expand-lg bg-light navbar-expand-md navigation-clean-search">
-          <div class="container">
-            <a class="navbar-brand" href="../index.html">
-              <img src="../../../public/client//images/logo-locationtour.png" class="logo" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navcol-1" aria-controls="navcol-1" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"><i class="fa-solid fa-bars icon-bars"></i></span>
-            </button>
-              <div class="collapse navbar-collapse justify-content-between" id="navcol-1">
-                <div class="none"></div>
-                  <ul class="nav navbar-nav">
-                      <li class="nav-item" role="presentation"><a class="nav-link" href="../view/product-list.html">Tour</a></li>
-                      <li class="nav-item" role="presentation"><a class="nav-link" href="#">Về chúng tôi</a></li>
-                      <li class="nav-item" role="presentation"><a class="nav-link" href="../view/contact.html">Liên hệ</a></li>
-                      <li class="nav-item" role="presentation"><a class="nav-link" href="../view/faq.html">FAQ</a></li>
-                      <!-- <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Services</a>
-                          <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">Logo design</a><a class="dropdown-item" role="presentation" href="#">Banner design</a><a class="dropdown-item" role="presentation" href="#">content writing</a></div>
-                      </li> -->
-                  </ul>
-                  <!-- <form class="form-inline mr-auto" target="_self">
-                      <div class="form-group"><label for="search-field"><i class="fa fa-search text-white"></i></label><input class="form-control search-field" type="search" id="search-field" name="search"></div>
-                  </form> -->
-                  <div>
-                    <span class="navbar-text"> <a class="login text-dark" href="./login.html">Đăng nhập</a></span>
-                    <a class="btn btn-primary action-button" role="button" href="./register.html">Đăng ký</a>
-                  </div>
-                </div>
-          </div>
-      </nav>
+      <!-- Header -->
+    <?php include_once '../layouts/header.php' ?>
   </div>
     <div class="container">
       <div class="card w-100">
@@ -106,74 +79,8 @@
           </div>
       </div>
   </div>
-  <footer class="bg-white">
-    <div class="container py-5">
-      <div class="row py-3">
-
-        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-          <h6 class="text-uppercase font-weight-bold mb-4">Về chúng tôi</h6>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-2"><a href="#" class="text-muted">Sự Kiện</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Tour</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Nội dung</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Giới thiệu</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-          <h6 class="text-uppercase font-weight-bold mb-4">Trợ giúp</h6>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-2"><a href="#" class="text-muted">Thanh toán</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Dịch vụ</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Cancellation</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Returns</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-          <h6 class="text-uppercase font-weight-bold mb-4">Chinh sách</h6>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-2"><a href="#" class="text-muted">Chính sách hoàn trả</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Điều khoản sử dụng</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Bảo mật</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Điều khoản</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-          <h6 class="text-uppercase font-weight-bold mb-4">Công ty</h6>
-          <ul class="list-unstyled mb-0">
-            <li class="mb-2"><a href="#" class="text-muted">Đăng nhập</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Đăng ký</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">sơ đồ sử dụng</a></li>
-            <li class="mb-2"><a href="#" class="text-muted">Các dịch vụ</a></li>
-          </ul>
-        </div>
-
-        <div class="col-lg-4 col-md-6 mb-lg-0">
-          <h6 class="text-uppercase font-weight-bold mb-4">Địa chỉ văn phòng</h6>
-          <p class="text-muted mb-4">Khu II, Đ. 3/2, Xuân Khánh, Ninh Kiều, Cần Thơ. Điện thoại: 0379830470 - 0948935476</p>
-          <ul class="list-inline mt-4">
-            <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fab  fa-2x fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fab fa-2x fa-facebook-f"></i></a></li>
-            <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fab fa-2x fa-instagram"></i></a></li>
-            <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fab fa-2x fa-youtube"></i></a></li>
-            <li class="list-inline-item"><a href="#" target="_blank" title="vimeo"><i class="fab fa-2x fa-google"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="container text-center">
-      <p class="text-muted mb-0 py-2">&copy; Nguyễn Phúc Vinh - Nguyễn Thái Sơn</p>
-    </div>
-    <!-- <hr class="p-0 m-0 b-0">
-    <div class="bg-light py-2">
-      <div class="container text-center">
-        <p class="text-muted mb-0 py-2">&copy; 2022 Design by student CTU. All rights reserved.</p>
-      </div>
-    </div> -->
-
-  </footer>
+      <!-- Footer -->
+      <?php include_once '../layouts/footer.php' ?>
   </div>
   <a class="phone-call d-block btn btn-secondary" href="tel:0398033985">
     <i class="fa-solid fa-phone d-block" style="font-size: 1.4rem; margin-top: 0.4rem;"></i>
